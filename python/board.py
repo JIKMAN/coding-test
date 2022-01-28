1543e77672520b68e1117e287420ad9641b0f81e
Jungik <wjddlr0303@gmail.com>
2022-01-28 21:35:02 +0900

    board

---
+++ python/board.py
@@ -0,0 +1,11 @@
+board = [[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]]
+board[0][0] = 100
+if board[0][1] == 0:
+    board[0][1] = 100
+if board[1][0] == 0:
+    board[1][0] = 100
+ 
+
+# def check(x):
+
+print(board)
