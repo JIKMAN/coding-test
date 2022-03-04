1bad815f702f0dc787a23315693cd597083c76ec
Jungik <wjddlr0303@gmail.com>
2022-03-04 21:32:16 +0900

    9095

---
+++ python/9095.py
@@ -0,0 +1,18 @@
+# 9095 : https://www.acmicpc.net/problem/9095
+
+
+def solution(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+    else:
+        return solution(n-1) + solution(n-2) + solution(n-3)
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    print(solution(n))
