12688e9a8b0b7f90add74651828cf863f32754bc
Jungik <wjddlr0303@gmail.com>
2022-03-18 23:07:50 +0900

    72414

---
+++ python/72414.py
@@ -0,0 +1,56 @@
+## https://programmers.co.kr/learn/courses/30/lessons/72414?language=python3
+
+
+
+play_time = "50:00:00"
+adv_time = "50:00:00"
+logs = ["15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"]
+
+
+def solution(play_time, adv_time, logs):
+    play_time = str_to_int(play_time)
+    adv_time = str_to_int(adv_time)               
+    all_time = [0 for i in range(play_time + 1)]
+
+    for l in logs:
+        start, end = l.split('-')
+        start = str_to_int(start)
+        end = str_to_int(end)
+        all_time[start] += 1
+        all_time[end] -= 1
+
+    for i in range(1, len(all_time)):
+        all_time[i] = all_time[i] + all_time[i - 1]
+
+    for i in range(1, len(all_time)):
+        all_time[i] = all_time[i] + all_time[i - 1]
+
+    most_view = 0
+    max_time = 0                          
+    for i in range(adv_time - 1, play_time):
+        if i >= adv_time:
+            if most_view < all_time[i] - all_time[i - adv_time]:
+                most_view = all_time[i] - all_time[i - adv_time]
+                max_time = i - adv_time + 1
+        else:
+            if most_view < all_time[i]:
+                most_view = all_time[i]
+                max_time = i - adv_time + 1
+
+    return int_to_str(max_time)
+
+
+def str_to_int(time):
+    h, m, s = time.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+
+def int_to_str(time):
+    h = time // 3600
+    h = '0' + str(h) if h < 10 else str(h)
+    time = time % 3600
+    m = time // 60
+    m = '0' + str(m) if m < 10 else str(m)
+    time = time % 60
+    s = '0' + str(time) if time < 10 else str(time)
+    return h + ':' + m + ':' + s
