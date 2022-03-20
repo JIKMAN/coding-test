69810ca8c941038be2f51ff38240adbeb1bd8b41
Jungik <wjddlr0303@gmail.com>
2022-03-20 18:12:00 +0900

    42627

---
+++ python/42627.py
@@ -0,0 +1,25 @@
+## https://programmers.co.kr/learn/courses/30/lessons/42627?language=python3
+
+import heapq
+
+def solution(jobs):
+    answer = 0
+    time = 0
+    start = -1
+    count = 0
+    h = []
+    while count < len(jobs):
+        for job in jobs:
+            if start < job[0] <= time:
+                heapq.heappush(h, [job[1], job[0]])
+                
+        if h:
+            cur = heapq.heappop(h)        
+            start = time
+            time += cur[0]
+            answer += (time - cur[1])
+            count += 1
+        else:
+            time += 1
+
+    return int(answer / len(jobs))
